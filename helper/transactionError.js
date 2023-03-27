44920cf30078cad69aba9fcfef6c1c156a8ae1a0
cherrybytedev <89452621+walifile@users.noreply.github.com>
2023-03-27 15:03:34 +0500

    fix : error-name

--- helper/transactionError.js
+++ helper/transactionError.js
@@ -1,17 +1,25 @@
 import { Toast } from "../components/shared/Toast";
 export const transactionErrorHanlder = (error, type) => {
-  console.log(type, "hello");
+  console.log(error, "hello");
   if (error.code == -3) {
     Toast(
       "error",
       "Sorry, it looks like the pop-up window for your wallet was closed before the process could be completed. Please reopen the wallet and try again."
     );
-  } else if (error.message.includes("Expected address with network id 0")) {
+  } else if (error.code == 2) {
+    Toast(
+      "error",
+      "Transaction cancelled. Your request to complete the transaction has been cancelled."
+    );
+  } else if (
+    error?.message &&
+    error?.message?.includes("Expected address with network id 0")
+  ) {
     Toast(
       "error",
       "Sorry, it looks like you're currently connected to the mainnet. Please switch your network to preprod in order to continue."
     );
-  } else if (e?.info === "Wallet could not send the tx.") {
+  } else if (error?.info === "Wallet could not send the tx.") {
     Toast(
       "error",
       "Your previous transaction is being validated please try again later."
