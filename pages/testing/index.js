6e7e985787b5cea76a0535b33b2d75810c18276a
wali ahmad <141010269+walicherry@users.noreply.github.com>
2023-08-18 14:45:59 +0500

    feat : add buy

--- pages/testing/index.js
+++ pages/testing/index.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import { BlockfrostProvider } from "@meshsdk/core";
 import { network_key } from "../../base_network";
 import { market } from "../../config";
-import Listing from "/components/testing/Listing.jsx"
+import Listing from "/components/testing/Listing.jsx";
 import {
   calculatePolicyHash,
   decodeAssetName,
@@ -21,7 +21,8 @@ import {
 import { Address, BaseAddress } from "@emurgo/cardano-serialization-lib-asmjs";
 const Testing = () => {
   const [nfts, setNfts] = useState([]);
-
+  const [selectedNFTs, setSelectedNFTs] = useState([]); // Track selected NFTs
+  console.log(selectedNFTs, "selectedNFTs");
   const assets = useAssets();
   const blockfrostProvider = new BlockfrostProvider(network_key);
   useEffect(() => {
@@ -65,15 +66,71 @@ const Testing = () => {
     // console.log(addresses, "addressesaddressesaddresses");
     // console.log(api, "sellNftsellNftsellNft");
   };
-  const sellNft = async (providerInstance, asset) => {
+  // const sellNft = async (providerInstance, asset) => {
+  //   console.log(providerInstance, "providerInstance");
+  //   console.log(asset, "asset");
+  //   const addresses = await providerInstance.getUsedAddresses();
+  //   console.log(addresses, "addressesaddressesaddresses");
+
+  //   console.log(
+  //     Address.from_bytes(Uint8Array.from(Buffer.from(addresses[0], "hex")))
+  //   );
+
+  //   const sellerAddr = BaseAddress.from_address(
+  //     Address.from_bytes(Uint8Array.from(Buffer.from(addresses[0], "hex")))
+  //   );
+  //   console.log("sellerAddr", sellerAddr);
+  //   const sellerPkh = Buffer.from(
+  //     sellerAddr.payment_cred().to_keyhash().to_bytes()
+  //   ).toString("hex");
+  //   const sellerStakeKey = Buffer.from(
+  //     sellerAddr.stake_cred().to_keyhash().to_bytes()
+  //   ).toString("hex");
+  //   const body = {
+  //     selections: await providerInstance.getUtxos(),
+  //     outputs: [
+  //       {
+  //         address: market.address,
+  //         value: `${asset.policyId}.${asset.name}`,
+  //         datum: {
+  //           fields: [
+  //             {
+  //               fields: [
+  //                 { fields: [{ bytes: `${sellerPkh}` }], constructor: 0 }, // pubkeyhash
+  //                 {
+  //                   fields: [
+  //                     {
+  //                       fields: [
+  //                         {
+  //                           fields: [{ bytes: `${sellerStakeKey}` }],
+  //                           constructor: 0,
+  //                         },
+  //                       ],
+  //                       constructor: 0,
+  //                     },
+  //                   ],
+  //                   constructor: 0,
+  //                 }, // stakekeyHash
+  //               ],
+  //               constructor: 0,
+  //             },
+  //             // sellAmount: "",
+  //             { int: Math.round(parseFloat(40) * 1e6) },
+  //           ],
+  //           constructor: 0,
+  //         },
+  //       },
+  //     ],
+  //   };
+  //   callKuberAndSubmit(providerInstance, JSON.stringify(body));
+  // };
+
+  const sellNft = async (providerInstance, selectedNFTs) => {
     console.log(providerInstance, "providerInstance");
-    console.log(asset, "asset");
+    console.log(selectedNFTs, "selectedNFTs");
+
     const addresses = await providerInstance.getUsedAddresses();
     console.log(addresses, "addressesaddressesaddresses");
-
-    console.log(
-      Address.from_bytes(Uint8Array.from(Buffer.from(addresses[0], "hex")))
-    );
 
     const sellerAddr = BaseAddress.from_address(
       Address.from_bytes(Uint8Array.from(Buffer.from(addresses[0], "hex")))
@@ -85,43 +142,46 @@ const Testing = () => {
     const sellerStakeKey = Buffer.from(
       sellerAddr.stake_cred().to_keyhash().to_bytes()
     ).toString("hex");
-    const body = {
-      selections: await providerInstance.getUtxos(),
-      outputs: [
-        {
-          address: market.address,
-          value: `${asset.policyId}.${asset.name}`,
-          datum: {
+
+    const outputs = selectedNFTs.map((asset) => ({
+      address: market.address,
+      value: `${asset.policyId}.${asset.name}`,
+      datum: {
+        fields: [
+          {
             fields: [
+              { fields: [{ bytes: `${sellerPkh}` }], constructor: 0 }, // pubkeyhash
               {
                 fields: [
-                  { fields: [{ bytes: `${sellerPkh}` }], constructor: 0 }, // pubkeyhash
                   {
                     fields: [
                       {
-                        fields: [
-                          {
-                            fields: [{ bytes: `${sellerStakeKey}` }],
-                            constructor: 0,
-                          },
-                        ],
+                        fields: [{ bytes: `${sellerStakeKey}` }],
                         constructor: 0,
                       },
                     ],
                     constructor: 0,
-                  }, // stakekeyHash
+                  },
                 ],
                 constructor: 0,
-              },
-              // sellAmount: "",
-              { int: Math.round(parseFloat(40) * 1e6) },
+              }, // stakekeyHash
             ],
             constructor: 0,
           },
-        },
-      ],
+          { int: Math.round(parseFloat(40) * 1e6) },
+        ],
+        constructor: 0,
+      },
+    }));
+
+    const selections = await providerInstance.getUtxos();
+    console.log(selections, "selections");
+    const body = {
+      selections,
+      outputs,
     };
-    callKuberAndSubmit(providerInstance, JSON.stringify(body));
+    console.log(body, "body");
+    // callKuberAndSubmit(providerInstance, JSON.stringify(body));
   };
   const handleAddButtonClick = (index, price) => {
     // Save the price and set isSelling back to false
@@ -132,47 +192,79 @@ const Testing = () => {
   };
 
   // console.log(nfts, "assetsassetsassetsassets");
+  const handleCheckboxChange = (selectedNFT) => {
+    setSelectedNFTs((prevSelectedNFTs) => {
+      const nftIndex = prevSelectedNFTs.findIndex(
+        (nft) => nft.name === selectedNFT.name
+      );
+      if (nftIndex !== -1) {
+        // If the NFT is already selected, remove it from the list
+        return prevSelectedNFTs.filter((nft, index) => index !== nftIndex);
+      } else {
+        // Otherwise, add it to the list
+        return [...prevSelectedNFTs, selectedNFT];
+      }
+    });
+  };
+
+  const handleSellCollectionClick = () => {
+    // Loop through selectedNFTs and call sellNft for each selected NFT
+    sellNft(instance, selectedNFTs);
+  };
 
   return (
     <>
-    <Box
-      sx={{
-        height: "40vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <p onClick={() => connectWallet()}>connect wallet</p>
-      {nfts.map((nft, index) => (
-        <div key={index}>
-          <h2>{nft.name}</h2>
-          <img src={`https://ipfs.io/ipfs${nft.image}`} alt={nft.name} style={{width:"100px" , height:"100px"}} />
-          <p>{nft.description}</p>
-          {nft.isSelling ? (
-            <>
-              <input
-                type="text"
-                value={nft.price}
-                onChange={(e) => {
-                  const updatedNfts = [...nfts];
-                  updatedNfts[index].price = e.target.value;
-                  setNfts(updatedNfts);
-                }}
-              />
-              <button onClick={() => handleAddButtonClick(index, nft.price)}>
-                Add
-              </button>
-            </>
-          ) : (
-            <button onClick={() => sellNft(instance, nft)}>Sell</button>
-            // <button onClick={() => handleSellButtonClick(index)}>Sell</button>
-          )}
-        </div>
-      ))}
-    </Box>
-    <p>hello</p>
-    <Listing instance={instance}/>
+      <Box
+        sx={{
+          height: "40vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <p onClick={() => connectWallet()}>connect wallet</p>
+        <button onClick={handleSellCollectionClick}>Sell Collection</button>
+
+        {nfts.map((nft, index) => (
+          <div key={index}>
+            <h2>{nft.name}</h2>
+            <img
+              src={`https://ipfs.io/ipfs${nft.image}`}
+              alt={nft.name}
+              style={{ width: "100px", height: "100px" }}
+            />
+            <p>{nft.description}</p>
+            <input
+              type="checkbox"
+              checked={selectedNFTs.some(
+                (selectedNFT) => selectedNFT.name === nft.name
+              )}
+              onChange={() => handleCheckboxChange(nft)}
+            />
+            {nft.isSelling ? (
+              <>
+                <input
+                  type="text"
+                  value={nft.price}
+                  onChange={(e) => {
+                    const updatedNfts = [...nfts];
+                    updatedNfts[index].price = e.target.value;
+                    setNfts(updatedNfts);
+                  }}
+                />
+                <button onClick={() => handleAddButtonClick(index, nft.price)}>
+                  Add
+                </button>
+              </>
+            ) : (
+              <button onClick={() => sellNft(instance, nft)}>Sell</button>
+              // <button onClick={() => handleSellButtonClick(index)}>Sell</button>
+            )}
+          </div>
+        ))}
+      </Box>
+      <p>hello</p>
+      <Listing instance={instance} />
     </>
   );
 };
