5fbce958996a79ca372f232554d53e5d7dea0f88
wali ahmad <141010269+walicherry@users.noreply.github.com>
2023-08-23 14:33:18 +0500

    update : backend endpoint

--- config/endpoint.js
+++ config/endpoint.js
@@ -3,5 +3,7 @@
 // export const baseURL = "http://192.168.1.47:4001/api";
 //
 // export const baseURL = "http://192.168.100.26:3002/api/v1";
-export const baseURL = "https://backend-cnft.thecbt.cyou/api";
+export const baseURL = "https://backend-cnft.thecbt.live/api";
+// export const baseURL = "https://backend-cnft.thecbt.cyou/api";
 // export const baseURL = "http://localhost:4001/api";
+// cyou live s
