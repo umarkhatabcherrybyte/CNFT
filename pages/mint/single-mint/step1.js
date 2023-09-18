09a1b9954814e02ede2423848460019cdc9f1409
wali ahmad <89452621+walifile@users.noreply.github.com>
2023-09-18 16:30:46 +0500

    feat : add audio or video for minting

--- pages/mint/single-mint/step1.js
+++ pages/mint/single-mint/step1.js
@@ -12,22 +12,26 @@ import {
 import { Toast } from "/components/shared/Toast";
 import { useRouter } from "next/router";
 import { create } from "ipfs-http-client";
+import FullSreenLoader from "/components/shared/FullScreenLoader";
 import { mintSingleStep2 } from "/components/Routes/constants";
 import CaptionHeading from "/components/shared/headings/CaptionHeading";
 const SingleMint = () => {
   const router = useRouter();
 
   const [mintFile, setMintFile] = useState(null);
+  const [featureImage, setFeatureImage] = useState(null);
   const [mintFileFormat, setMintFileFormat] = useState("");
   const [isUploading, setIsUploading] = useState(false);
-
+  console.log(mintFileFormat, "mintFileFormat");
   const onSelectFile = ({ currentTarget: input }) => {
     if (input.files && input.files[0]) {
       const files = input.files[0];
       setMintFile(files);
+      setMintFileFormat("");
       const name = files?.name.toLowerCase();
       if (name.match(/\.(jpg|jpeg|png|gif)$/)) {
         setMintFileFormat("image");
+        setFeatureImage(null);
       } else if (name.match(/\.(mp3)$/)) {
         setMintFileFormat("audio");
       } else if (name.match(/\.(mp4)$/)) {
@@ -38,33 +42,77 @@ const SingleMint = () => {
     }
   };
 
+  const onFeatureImageSelect = ({ currentTarget: input }) => {
+    if (input.files && input.files[0]) {
+      const files = input.files[0];
+      setFeatureImage(files);
+    }
+  };
+
   const onNextButton = async () => {
+    setIsUploading(true);
+    const projectId = "2IAoACw6jUsCjy7i38UO6tPzYtX";
+    const projectSecret = "136393a5b7f4e47a9e153a88eb636003";
+    const auth = `Basic ${Buffer.from(`${projectId}:${projectSecret}`).toString(
+      "base64"
+    )}`;
+    const client = create({
+      host: "ipfs.infura.io",
+      port: 5001,
+      protocol: "https",
+      headers: {
+        authorization: auth,
+      },
+    });
     if (mintFile !== null && mintFileFormat != "invalid") {
-      setIsUploading(true);
       try {
-        const projectId = "2IAoACw6jUsCjy7i38UO6tPzYtX";
-        const projectSecret = "136393a5b7f4e47a9e153a88eb636003";
-        const auth = `Basic ${Buffer.from(
-          `${projectId}:${projectSecret}`
-        ).toString("base64")}`;
-        const client = create({
-          host: "ipfs.infura.io",
-          port: 5001,
-          protocol: "https",
-          headers: {
-            authorization: auth,
-          },
-        });
-        const uploaded_image = await client.add(mintFile);
-        console.log(uploaded_image);
-        if (uploaded_image) {
-          Toast("success", "Uploaded Successfully");
+        if (mintFileFormat === "image") {
+          const uploaded_image = await client.add(mintFile);
+          if (uploaded_image) {
+            console.log(uploaded_image);
+            Toast("success", "Uploaded Successfully");
+            setIsUploading(false);
+            window.localStorage.setItem("img", uploaded_image.path);
+            window.localStorage.setItem("file_mimeType", mintFile.type);
+          }
+
+          // router.push(mintSingleStep2);
+        } else if (mintFileFormat === "video" || mintFileFormat === "audio") {
+          // console.log("audio ya video ha ");
+
+          if (!featureImage) {
+            console.log("feature image ni ha ");
+            Toast("error", "Please Select a featured image and try again.");
+            return;
+          } else {
+            // console.log(featureImage, "featureImage");
+            // console.log(mintFile, "featureImage");
+            // const [uploadedFeatureFile, uploadedMintFile] = await Promise.all([
+            //   client.add(featureImage),
+            //   client.add(mintFile),
+            // ]);
+            const uploadedFeatureFile = await client.add(featureImage);
+            const uploadedMintFile = await client.add(mintFile);
+            console.log(uploadedFeatureFile, "uploadedFeatureFile");
+            console.log(uploadedMintFile, "uploadedMintFile");
+            if (uploadedFeatureFile && uploadedMintFile) {
+              Toast("success", "Files Uploaded Successfully");
+              setIsUploading(false);
+              // window.localStorage.setItem("img", uploaded_image.path);
+              // window.localStorage.setItem("file_mimeType", mintFile.type);
+              // Save file paths or other necessary information
+            } else {
+              Toast("error", "Failed to Upload Files");
+              setIsUploading(false);
+            }
+          }
+        } else {
+          // Handle unsupported formats
+          Toast("error", "Unsupported File Format");
           setIsUploading(false);
-          window.localStorage.setItem("img", uploaded_image.path);
-          window.localStorage.setItem("file_mimeType", mintFile.type);
-          router.push(mintSingleStep2);
         }
       } catch (error) {
+        console.log(error, "errorerror");
         setIsUploading(false);
         Toast("error", "Uploading failed.");
       }
@@ -77,6 +125,7 @@ const SingleMint = () => {
   return (
     <SingleMintStyled>
       <Container>
+        {isUploading && <FullSreenLoader />}
         <Heading />
         <Box
           className="text_white"
@@ -88,89 +137,121 @@ const SingleMint = () => {
             // mb: 8,
           }}
         >
-          {!isUploading ? (
-            <>
-              <Grid container spacing={{ md: 3, xs: 3 }}>
-                <Grid item md={4} xs={12}>
-                  <Box
-                    className="upload_panel"
-                    sx={{ height: { xs: "auto", md: "100%" } }}
-                  >
-                    <img src="/images/Upload_icon.png" alt="mint" />
-                  </Box>
-                  <Box
-                    sx={{ py: 3, display: "flex", justifyContent: "center" }}
-                  >
-                    <Button className="btn2" component="label">
-                      <input
-                        hidden
-                        type="file"
-                        onChange={(e) => onSelectFile(e)}
-                      />
-                      Select File
-                    </Button>
-                  </Box>
-                </Grid>
-                <Grid item md={4} xs={12}></Grid>
-                <Grid item md={4} xs={12}>
-                  <Box
-                    className="upload_panel bg"
-                    sx={{ height: { xs: "auto", md: "100%" } }}
-                  >
-                    {mintFile ? (
-                      <>
-                        {mintFileFormat === "image" && (
-                          <img src={URL.createObjectURL(mintFile)} alt="mint" />
-                        )}
-                        {mintFileFormat === "video" && (
-                          <video controls>
-                            <source
-                              src={URL.createObjectURL(mintFile)}
-                              type="video/mp4"
-                            ></source>
-                          </video>
-                        )}
-                        {mintFileFormat === "audio" && (
-                          <audio controls>
-                            <source
-                              src={URL.createObjectURL(mintFile)}
-                              type="audio/mp3"
-                            ></source>
-                          </audio>
-                        )}
-                        {mintFileFormat === "invalid" && (
-                          <Typography variant="h6" sx={{ color: "red" }}>
-                            Invalid Format
-                          </Typography>
-                        )}
-                      </>
-                    ) : (
-                      <img src="/images/Image_icon.png" alt="mint" />
-                    )}
-                  </Box>
-                  <Typography
-                    variant="h6"
-                    sx={{ py: 2, textAlign: "center" }}
-                    component="div"
-                  >
-                    Preview
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Box sx={{ display: "flex", justifyContent: "center", pt: 10 }}>
-                <Button className="btn2" onClick={onNextButton}>
-                  Next
+          <Grid container spacing={{ md: 3, xs: 3 }}>
+            <Grid item md={4} xs={12}>
+              <Box
+                className="upload_panel"
+                // sx={{ height: { xs: "auto", md: "100%" } }}
+                sx={{ height: { xs: "auto", md: "auto" } }}
+              >
+                <img src="/images/Upload_icon.png" alt="mint" />
+              </Box>
+              <Box sx={{ py: 3, display: "flex", justifyContent: "center" }}>
+                <Button className="btn2" component="label">
+                  <input
+                    hidden
+                    type="file"
+                    onChange={(e) => onSelectFile(e)}
+                    accept="image/*, audio/*, video/*"
+                  />
+                  Select File
                 </Button>
               </Box>
-            </>
-          ) : (
-            <Box className="flex" sx={{ flexDirection: "column" }}>
-              <CircularProgress sx={{ color: "#fff" }} />
-              <Box sx={{ py: 3 }}>
-                <CaptionHeading heading="Please wait for a while ..." />
+            </Grid>
+            <Grid item md={4} xs={12}>
+              {mintFileFormat === "audio" ||
+                (mintFileFormat === "video" && (
+                  <>
+                    <Box
+                      className="upload_panel bg"
+                      sx={{
+                        height: {
+                          xs: "auto",
+                          md: "21rem",
+                        },
+                      }}
+                    >
+                      {featureImage ? (
+                        <img
+                          src={URL.createObjectURL(featureImage)}
+                          alt="mint"
+                        />
+                      ) : (
+                        <img src="/images/Image_icon.png" alt="mint" />
+                      )}
+                    </Box>
+                    <Box
+                      sx={{
+                        py: 3,
+                        display: "flex",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Button className="btn2" component="label">
+                        <input
+                          hidden
+                          type="file"
+                          onChange={(e) => onFeatureImageSelect(e)}
+                          accept="image/*"
+                        />
+                        Select Feature Image
+                      </Button>
+                    </Box>
+                  </>
+                ))}
+            </Grid>
+            <Grid item md={4} xs={12}>
+              <Box
+                className="upload_panel bg"
+                sx={{ height: { xs: "auto", md: "21rem" } }}
+              >
+                {mintFile ? (
+                  <>
+                    {mintFileFormat === "image" && (
+                      <img src={URL.createObjectURL(mintFile)} alt="mint" />
+                    )}
+                    {mintFileFormat === "video" && (
+                      <video controls>
+                        <source
+                          src={URL.createObjectURL(mintFile)}
+                          type="video/mp4"
+                        ></source>
+                      </video>
+                    )}
+                    {mintFileFormat === "audio" && (
+                      <audio controls>
+                        <source
+                          src={URL.createObjectURL(mintFile)}
+                          type="audio/mp3"
+                        ></source>
+                      </audio>
+                    )}
+
+                    {mintFileFormat === "invalid" && (
+                      <Typography variant="h6" sx={{ color: "red" }}>
+                        Invalid Format
+                      </Typography>
+                    )}
+                  </>
+                ) : (
+                  <img src="/images/Image_icon.png" alt="mint" />
+                )}
               </Box>
-            </Box>
-          )}
+
+              <Typography
+                variant="h6"
+                sx={{ py: 2, textAlign: "center" }}
+                component="div"
+              >
+                Preview
+              </Typography>
+            </Grid>
+          </Grid>
+          <Box sx={{ display: "flex", justifyContent: "center", pt: 10 }}>
+            <Button className="btn2" onClick={onNextButton}>
+              Next
+            </Button>
+          </Box>
         </Box>
       </Container>
     </SingleMintStyled>
