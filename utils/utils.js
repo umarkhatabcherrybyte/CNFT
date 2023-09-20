2d8782bac0fa81a77008d97216741ad942ade26f
wali ahmad <89452621+walifile@users.noreply.github.com>
2023-09-20 17:57:55 +0500

    feat : add upload to IPFS function

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,28 @@
+import { create } from "ipfs-http-client";
+import { ipfsConfig } from "../config/ipfsConfig";
+const { projectId, projectSecret } = ipfsConfig; // Use the configuration
+const auth = `Basic ${Buffer.from(`${projectId}:${projectSecret}`).toString(
+  "base64"
+)}`;
+
+const ipfsClient = create({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
+  headers: {
+    authorization: auth,
+  },
+});
+export const handleFileUpload = async (file) => {
+  try {
+    const uploaded_file = await ipfsClient.add(file);
+    return uploaded_file;
+  } catch (error) {
+    console.log(error);
+    // throw new Error("Failed to upload image to IPFS");
+  }
+};
 export const isVideoOrIsAudio = (file) => {
-  // console.log(file.media_type, "fdfds");
   return (
     file.media_type == "audio/mpeg" ||
     file.media_type == "audio/mp4" ||
